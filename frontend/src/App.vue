9830abdb94f52061eca1deeeab815ca8254d5616
Marcel Witoschek <marcel.witoschek@gdata.de>
2024-09-24 14:10:07 +0200

    add empty frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import * as bootstrap from "bootstrap";
+</script>
+
+<template>
+  <div class="app-container">
+    <header></header>
+    <main>
+      <router-view />
+    </main>
+
+    <footer></footer>
+  </div>
+</template>
+
+<style>
+@import "@/assets/main.css";
+</style>
---
+++ frontend/src/pages/Index.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>Stuff</template>
+
+<style scoped></style>
